30f2fe4bda7d3b22b7f18be33567fa4363429c5b
James Kyle <me@thejameskyle.com>
2015-11-23 23:50:40 -0800

    Upgrade to Babel 6

--- test/fixtures/tagged-template-expression/expected.js
+++ test/fixtures/tagged-template-expression/expected.js
@@ -1,11 +1,9 @@
-'use strict';
-
-var _Handlebars = require('handlebars/runtime')['default'];
+import _Handlebars2 from 'handlebars/runtime';
 
 _Handlebars.template({
-  'compiler': [6, '>= 2.0.0-beta.1'],
-  'main': function main(depth0, helpers, partials, data) {
-    return 'Hello World!';
+  "compiler": [6, ">= 2.0.0-beta.1"],
+  "main": function (depth0, helpers, partials, data) {
+    return "Hello World!";
   },
-  'useData': true
+  "useData": true
 });
