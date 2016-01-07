a181e939edfa8e08f198aaf52796eda4cf1bfbe3
Mark Wubben <mark@novemberborn.net>
2016-01-07 17:55:34 +0000

    handlebars@^4.0.5

--- test/fixtures/tagged-template-expression/expected.js
+++ test/fixtures/tagged-template-expression/expected.js
@@ -1,8 +1,8 @@
 import _Handlebars2 from 'handlebars/runtime';
 
 _Handlebars.template({
-  "compiler": [6, ">= 2.0.0-beta.1"],
-  "main": function (depth0, helpers, partials, data) {
+  "compiler": [7, ">= 4.0.0"],
+  "main": function (container, depth0, helpers, partials, data) {
     return "Hello World!";
   },
   "useData": true
